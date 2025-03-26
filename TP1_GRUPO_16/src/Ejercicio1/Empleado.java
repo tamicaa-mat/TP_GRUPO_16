190765ec78c1d526011d5ad5e3a0ae61a30d6c0b
tamicaa-mat <tamicaa.mat@gmail.com>
2025-03-26 14:55:08 -0300

    Finalizado merge de master a main

---
+++ TP1_GRUPO_16/src/Ejercicio1/Empleado.java
@@ -0,0 +1,33 @@
+package Ejercicio1;
+
+public class Empleado {
+	
+	//Attributes
+	
+	int id;
+	String nombre;
+	int edad;
+	
+	//get set encapsulamiento
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+
+}
