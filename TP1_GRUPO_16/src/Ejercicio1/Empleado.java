c26565d7e29d4ef1edcb2c85927eb49f5518f2db
aruuleon <arieleonchang@gmail.com>
2025-03-28 22:00:55 -0300

    fix setnombre and setedad

--- TP1_GRUPO_16/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_16/src/Ejercicio1/Empleado.java
@@ -1,62 +1,45 @@
 package Ejercicio1;
 
 public class Empleado {
-	
+
 	//Attributes
-	
 	private final int ID; // ID Constante
 	private String nombre;
 	private int edad;
-	
-	private static int cont = 1000;
-	 
+	private static int cont = 1000; 
 
 	//constructores
-	public Empleado() 
-	{
+	public Empleado() {
 		this.ID = cont++;
-		this.nombre = "sin nombre";
-		this.edad = 99;
+		this.nombre = "";
+		this.edad = 21;
 	}
-	
-	public Empleado(String nombre,int edad) 
-	{
+	public Empleado(String nombre,int edad) {
 		this.ID = cont++;
-		this.nombre = nombre;
-		this.edad = edad;
-
+		setNombre(nombre);
+		setEdad(edad);
 	}
-	
-	
 	// metodos get set encapsulamiento(elimine el set de id, ya que debe autogenerarse)
-	
 	public String getNombre() {
 		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 	public int getEdad() {
 		return edad;
 	}
-	public void setEdad(int edad) {
-		this.edad = edad;
+	public void setNombre(String nombre) {
+		this.nombre = nombre.isEmpty() ? "sin nombre" : nombre;
 	}
-	
-	
+	public void setEdad(int edad) {
+		this.edad = edad < 0 ? 21 : edad;
+	}
 	 // Método que genera el próximo ID
     public static int devuelveProximoID() {
         return cont;
     }	
-
-    
 	//metodo tostring devuelve informacion del objeto
 	@Override
 	public String toString() { 
 		return "Empleado " + nombre + ", edad: " + edad + ", legajo: " + ID;
 	}
 	
-	
-
-
 }
